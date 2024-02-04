2e02e4f4adc0c6090fdd7702771bde48f8da28a3
miszek <szekalski.michal@gmail.com>
2024-02-04 22:52:09 +0100

    3 square snake is moving forward in one direction

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 snake = Snake()
 
+game_is_on = True
+while game_is_on:
+    snake.move()
+    screen.update()
 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,13 +1,48 @@
 from turtle import Turtle
+import time
+
+
+def create_turtle(pos_x, pos_y):
+    turtle = Turtle()
+    turtle.penup()
+    turtle.shape('square')
+    turtle.color('white')
+    turtle.setx(pos_x)
+    turtle.sety(pos_y)
+    return turtle
 
 
 class Snake:
-    positions = [(-40, 0), (-20, 0), (0, 0)]
+    turtles = []
+    direction = 0
 
     def __init__(self):
-        for position in self.positions:
-            turtle = Turtle()
-            turtle.shape('square')
-            turtle.color('white')
-            turtle.penup()
-            turtle.goto(position[0], position[1])
+        initial_positions = [(-40, 0), (-20, 0), (0, 0)]
+        for position in initial_positions:
+            pos_x = position[0]
+            pos_y = position[1]
+            self.turtles.append(create_turtle(pos_x, pos_y))
+
+    def move(self):
+        head_turtle = self.turtles[len(self.turtles) - 1]
+        head_turtle_pos_x = head_turtle.position()[0]
+        head_turtle_pos_y = head_turtle.position()[1]
+
+        if self.direction == 0:
+            head_turtle_pos_x += 20
+        elif self.direction == 90:
+            head_turtle_pos_y += 20
+        elif self.direction == 180:
+            head_turtle_pos_x -= 20
+        elif self.direction == 270:
+            head_turtle_pos_y -= 20
+
+        for i in range(0, len(self.turtles) - 1):
+            current_turtle = self.turtles[i]
+            next_turtle = self.turtles[i + 1]
+            current_turtle.setx(next_turtle.xcor())
+            current_turtle.sety(next_turtle.ycor())
+        head_turtle.setx(head_turtle_pos_x)
+        head_turtle.sety(head_turtle_pos_y)
+
+        time.sleep(0.2)
