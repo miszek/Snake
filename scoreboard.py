2ba5bbf5d7f720c33b7840dd136dd9306082ee4d
miszek <szekalski.michal@gmail.com>
2024-02-06 23:02:11 +0100

    scoreboard bug fix

--- scoreboard.py
+++ scoreboard.py
@@ -10,10 +10,13 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(x=0, y=260)
         self.hideturtle()
-        self.increase_score()
+        self.print_score()
 
     def increase_score(self):
-        self.clear()
         self.score += 1
+        self.print_score()
+
+    def print_score(self):
+        self.clear()
         writing = "Score: " + str(self.score)
         self.write(writing, align="center", font=('Arial', 24, 'normal'))
